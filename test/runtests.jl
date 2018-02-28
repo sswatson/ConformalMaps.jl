3ad7a5e3ee6055fe0f433007eb6bf82aa56e03f9
Samuel S. Watson <samuel.s.watson@gmail.com>
2018-02-28 09:25:12 -0500

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,23 +6,23 @@ vertices = [1.0  0.0;
             0.0  1.0;
             -1.0  0.0;
             0.0 -1.0]
-f = ConformalMap(vertices,0.0;resolution=100)
+f = ConformalMap(vertices,0.0)
 @test f(0.0) == 0
-@test f(0.1) ≈ 0.13109886582707742 + 6.127351451661506e-6im
+@test abs(f(0.1) - (0.13109886582707742 + 6.127351451661506e-6im)) < 1e-3 d
 
 vertices = [1, im, -1, -im] 
-f = ConformalMap(vertices,0.0im;resolution=100)
+f = ConformalMap(vertices,0.0im)
 g = inv(f)
 z = 0.1 + 0.1im; 
 @test g(f(z)) ≈ z 
 
 vertices = Complex{BigFloat}[1, im, -1, -im] 
-f = ConformalMap(vertices,0.0im;resolution=100)
+f = ConformalMap(vertices,0.0im)
 g = inv(f)
 z = 0.1 + 0.1im; 
 @test g(f(z)) ≈ z 
 
-@require Graphics2D begin
+@require AsyPlots begin
     slitdomain = [0.0, 0.495, 0.5 + 0.25*im, 0.505, 1.0, 1.0 + 1.0*im, im]
     f = ConformalMap(slitdomain,0.5+0.5im;resolution=40)
     g = inv(f)
